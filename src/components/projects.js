9623e6d126df4724511357496673d5183293ef00
reanzi <nziku99@gmail.com>
2018-05-28 08:23:45 +0300

    initial setup of tab navigation done

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
+// import React, { Component } from 'react';
 
-class Project extends Component {
+class Projects extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { activeTab: 0 };
+    }
+    toggleCategories() {
+        if (this.state.activeTab === 0) {
+            return (<div><h1>This is React</h1></div>)
+        } else if (this.state.activeTab === 1) {
+            return (
+                <div><h1>This is Django</h1></div>
+            )
+        } else if (this.state.activeTab === 2) {
+            return (<div><h1>This is Laravel</h1></div>)
+        } else if (this.state.activeTab === 3) {
+            return (<div><h1>This is VueJS</h1></div>)
+        }
+    }
+
+
+
     render() {
         return (
-            <div>
-                <h1>Project Page</h1>
+            <div className="demo-tabs">
+                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+                    <Tab>React</Tab>
+                    <Tab>Django</Tab>
+                    <Tab>Laravel</Tab>
+                    <Tab>VueJS</Tab>
+                </Tabs>
+                <section className="projects-grid">
+                    {this.toggleCategories()}
+                </section>
             </div>
-        )
+        );
     }
 }
 
-export default Project;
+export default Projects;
