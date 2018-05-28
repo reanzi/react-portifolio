7bfdaa5f7193aef08b993f83a0c2e4477d05ed37
reanzi <nziku99@gmail.com>
2018-05-28 11:59:52 +0300

    added card, react completed

--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-mdl';
-// import React, { Component } from 'react';
+import { Tabs, Tab, Grid, Cell, Card, CardText, CardTitle, CardActions, CardMenu, IconButton, Button } from 'react-mdl';
 
 class Projects extends Component {
     constructor(props) {
@@ -9,7 +8,59 @@ class Projects extends Component {
     }
     toggleCategories() {
         if (this.state.activeTab === 0) {
-            return (<div><h1>This is React</h1></div>)
+            return (
+                <div className="projects-grid">
+                    {/* 1st Project */}
+                    <Card shadow={5} style={{ width: '450px', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>1st Project</CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>gitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>Live Demo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+
+                    {/* 2nd Project */}
+                    <Card shadow={5} style={{ width: '450px', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>2nd Project</CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>gitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>Live Demo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                    {/* 3rd Project */}
+                    <Card shadow={5} style={{ width: '450px', margin: 'auto' }}>
+                        <CardTitle style={{ color: '#fff', height: '176px', background: 'url(http://www.getmdl.io/assets/demos/welcome_card.jpg) center / cover' }}>3rd Project</CardTitle>
+                        <CardText>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                            Mauris sagittis pellentesque lacus eleifend lacinia...
+                    </CardText>
+                        <CardActions border>
+                            <Button colored>gitHub</Button>
+                            <Button colored>CodePen</Button>
+                            <Button colored>Live Demo</Button>
+                        </CardActions>
+                        <CardMenu style={{ color: '#fff' }}>
+                            <IconButton name="share" />
+                        </CardMenu>
+                    </Card>
+                </div>
+            )
         } else if (this.state.activeTab === 1) {
             return (
                 <div><h1>This is Django</h1></div>
@@ -32,9 +83,15 @@ class Projects extends Component {
                     <Tab>Laravel</Tab>
                     <Tab>VueJS</Tab>
                 </Tabs>
-                <section className="projects-grid">
-                    {this.toggleCategories()}
-                </section>
+                {/* <section className="projects-grid"> */}
+                <Grid>
+                    <Cell col={12}>
+                        <div className="content">
+                            {this.toggleCategories()}
+                        </div>
+                    </Cell>
+                </Grid>
+                {/* </section> */}
             </div>
         );
     }
